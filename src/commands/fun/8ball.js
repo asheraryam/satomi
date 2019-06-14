251d49213e815a5fcd42dbcd9d71e555befcf2f6
Kyostra <christopherahouse@gmail.com>
2019-06-13 19:48:26 -0500

    0.20.0 moved 8ball answers into command

--- src/commands/fun/8ball.js
+++ src/commands/fun/8ball.js
@@ -1,5 +1,4 @@
 const { Command } = require('sylphy');
-const answers8ball = require('../../utils/answers8ball.js');
 
 class Eightball extends Command {
     constructor (...args) {
@@ -16,7 +15,31 @@ class Eightball extends Command {
 
     handle ({ args, client }, responder) {
         const question = args.question;
-        const answer = answers8ball.magicList[~~(Math.random() * answers8ball.magicList.length)];
+
+        const magicList = [
+            { name: 'It is certain' },
+            { name: 'It is decidedly so' },
+            { name: 'Without a doubt' },
+            { name: 'Yes definitely' },
+            { name: 'You may rely on it' },
+            { name: 'As I see it, yes' },
+            { name: 'Most likely' },
+            { name: 'Outlook good' },
+            { name: 'Yes' },
+            { name: 'Signs point to yes' },
+            { name: 'Reply hazy try again' },
+            { name: 'Ask again later' },
+            { name: 'Better not tell you now' },
+            { name: 'Cannot predict now' },
+            { name: 'Concentrate and ask again' },
+            { name: 'Dont count on it' },
+            { name: 'My reply is no' },
+            { name: 'My sources say no' },
+            { name: 'Outlook not so good' },
+            { name: 'Very doubtful' }
+        ];
+
+        const answer = magicList[~~(Math.random() * magicList.length)];
 
         return responder.send(' ', { embed: {
             color: client.satomiColor,
