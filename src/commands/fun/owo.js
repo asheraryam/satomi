5a22c7a85ac2c08b4688f80cf2c53fd3aa31e527
Kyostra <christopherahouse@gmail.com>
2020-06-01 09:02:00 -0500

    0.20.4 using a function instead of a package

--- src/commands/fun/owo.js
+++ src/commands/fun/owo.js
@@ -1,5 +1,4 @@
 const { Command } = require('sylphy');
-const owofy = require('owofy');
 
 class Owo extends Command {
     constructor (...args) {
@@ -16,6 +15,7 @@ class Owo extends Command {
     }
 
     handle ({ args, client, msg }, responder) {
+        const faces = ['(*^ω^)', '(◕‿◕✿)', '(◕ᴥ◕)', 'ʕ•ᴥ•ʔ', 'ʕ￫ᴥ￩ʔ', '(*^.^*)', 'owo', '(｡♥‿♥｡)', 'uwu', '(*￣з￣)', '>w<', '^w^', '(つ✧ω✧)つ', '(/ =ω=)/'];
         const phrase = args.phrase;
 
         if (!phrase) {
@@ -26,7 +26,22 @@ class Owo extends Command {
             } }).catch(this.logger.error);
         }
 
-        return responder.send(`${msg.author.mention} ${owofy(phrase)}`).catch(this.logger.error);
+        const owoPhrase = string => {
+            while (string.match(/!+/)) {
+                string = string.replace(/!+/, ` ${faces[Math.floor(Math.random() * faces.length)]} `)
+            }
+
+            return string
+                .replace(/(?:l|r)/g, 'w')
+                .replace(/(?:L|R)/g, 'W')
+                .replace(/n([aeiou])/g, 'ny$1')
+                .replace(/N([aeiou])/g, 'Ny$1')
+                .replace(/N([AEIOU])/g, 'NY$1')
+                .replace(/ove/g, 'uv')
+                .replace(/OVE/g, 'UV');
+        };
+
+        return responder.send(`${msg.author.mention} ${owoPhrase(phrase)}`).catch(this.logger.error);
     }
 }
 
